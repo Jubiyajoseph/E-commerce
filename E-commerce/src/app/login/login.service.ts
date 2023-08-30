3153c3c2b20570e637255c9eb0ba8df4f401336e
Jubiyajoseph <jubiyaaj15@gmail.com>
2023-08-30 18:09:13 +0530

    added loginpage

---
+++ E-commerce/src/app/login/login.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  constructor(private http: HttpClient) { }
+
+  private baseUrl= 'https://localhost:7271';
+
+  Login(username: string, password:string): Observable<any>
+  {
+    const body = {
+      username: username,
+      password: password
+    }
+    return this.http.post(`${this.baseUrl}/api/User/Login`, body)   
+  }
+
+}
