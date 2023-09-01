35016ccd1f2582e72145114c9011e9425eb574d5
Jubiyajoseph <jubiyaaj15@gmail.com>
2023-09-01 15:46:48 +0530

    added getProductquery

---
+++ E-Commerce_API/E-Commerce_API/Request/Query/GetProductQuery.cs
@@ -0,0 +1,11 @@
+﻿using E_Commerce.Model.Models.ProductsModel;
+using E_Commerce_API.Request.Command;
+using MediatR;
+
+namespace E_Commerce_API.Request.Query
+{
+    public class GetProductQuery: IRequest<AddProductCommand>
+    {
+        public int ProductID { get; set; }
+    }
+}
